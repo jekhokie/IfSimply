258dd030ffb4133749f32ed2a3ce1b860a174f8e
Justin Karimi <jekhokie@gmail.com>
2013-04-03 15:50:10 -0400

    Add Route for registration_notify
    Add a route for the registration_notify action via the home controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Ifsimply::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'registrations' }
 
   root :to => 'home#index'
 
   scope '/' do
-    match '/learn_more' => 'home#learn_more', :as => :learn_more
+    match '/learn_more'          => 'home#learn_more',          :as => :learn_more
+    match '/registration_notify' => 'home#registration_notify', :as => :registration_notify
   end
 end
