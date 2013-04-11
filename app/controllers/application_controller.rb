f9d104609da258fef10c62daa54a58ce181cdddb
Justin Karimi <jekhokie@gmail.com>
2013-04-11 13:34:21 -0400

    Add Unauthorized Response Handler for CanCan
    Add a 403 code and view return for the CanCan Unauthorized action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    @exception = exception
+    render :file => 'public/403', :status => 403
+  end
 end
