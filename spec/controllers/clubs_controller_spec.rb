948f807ebd8b7acc4cd0dfcd3fbd78fe7a705606
Justin Karimi <jekhokie@gmail.com>
2013-05-18 11:06:03 -0400

    Add Spec for Clubs Update Action
    Add a spec test for the Club update action.

--- spec/controllers/clubs_controller_spec.rb
+++ spec/controllers/clubs_controller_spec.rb
@@ -59,4 +59,53 @@ describe ClubsController do
       end
     end
   end
+
+  describe "PUT 'update'" do
+    before :each do
+      @request.env["devise.mapping"] = Devise.mappings[:users]
+      sign_in user
+    end
+
+    let(:club)     { user.clubs.first }
+    let(:new_name) { "Test Club" }
+
+    describe "for valid attributes" do
+      before :each do
+        put 'update', :id => club.id, :club => { :name => new_name }
+      end
+
+      it "returns http success" do
+        response.should be_success
+      end
+
+      it "returns the club" do
+        assigns(:club).should_not be_nil
+      end
+
+      it "assigns the new attributes" do
+        club.reload
+        club.name.should == new_name
+      end
+    end
+
+    describe "for invalid attributes" do
+      before :each do
+        @old_name = club.name
+        put 'update', :id => club.id, :club => { :name => "" }
+      end
+
+      it "returns http success" do
+        response.response_code.should == 422
+      end
+
+      it "returns the club" do
+        assigns(:club).should_not be_nil
+      end
+
+      it "does not update the attributes" do
+        club.reload
+        club.name.should == @old_name
+      end
+    end
+  end
 end
