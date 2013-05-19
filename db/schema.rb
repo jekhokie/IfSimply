f50ece5734067625f35fc10964689231269da55f
Justin Karimi <jekhokie@gmail.com>
2013-05-19 19:09:38 -0400

    Add Migration for Lesson Free Boolean
    Add a database migration to include the 'free' boolean for the Lesson
    model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130519202057) do
+ActiveRecord::Schema.define(:version => 20130519224224) do
 
   create_table "clubs", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20130519202057) do
     t.integer  "course_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "free"
   end
 
   add_index "lessons", ["course_id"], :name => "index_lessons_on_course_id"
