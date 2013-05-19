a2150fd60e47e800cb8e748a7f975dac42f6e66d
Justin Karimi <jekhokie@gmail.com>
2013-05-18 20:06:27 -0400

    Add Courses Controller with create Action
    Add a Courses controller to include the create action.

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,12 @@
+class CoursesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def create
+    @club   = Club.find params[:club_id]
+    @course = @club.courses.new
+    @course.assign_defaults
+    @course.save
+
+    render :edit
+  end
+end
