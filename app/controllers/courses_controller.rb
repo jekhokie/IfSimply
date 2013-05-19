0d0276627edca6b0d68344c84d3639699dc4021d
Justin Karimi <jekhokie@gmail.com>
2013-05-18 20:37:14 -0400

    Include Authn for Courses create Action
    Update the Courses controller to ensure the create action validates that
    the User is permitted to create the Course.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,9 @@ class CoursesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @club   = Club.find params[:club_id]
+    authorize! :create, Course
+
+    @club = Club.find params[:club_id]
     @course = @club.courses.new
     @course.assign_defaults
     @course.save
