1b29dfc11bce26027d1ed908cf96f9a9295fec12
Justin Karimi <jekhokie@gmail.com>
2013-05-18 20:00:07 -0400

    Add Courses Controller Spec
    Add specs for the CoursesController.

---
+++ spec/controllers/courses_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe CoursesController do
+  let(:user) { FactoryGirl.create :user }
+
+  describe "POST 'create'" do
+    before :each do
+      @request.env["devise.mapping"] = Devise.mappings[:users]
+      sign_in user
+
+      post 'create', :club_id => user.clubs.first.id
+    end
+
+    it "returns http success" do
+      response.should be_success
+    end
+
+    it "renders the edit view" do
+      response.should render_template('courses/edit')
+    end
+
+    it "returns the club" do
+      assigns(:club).should_not be_nil
+    end
+
+    it "returns the course belonging to the club" do
+      assigns(:course).should_not be_nil
+      assigns(:course).club.should == assigns(:club)
+    end
+
+    it "assigns the default title" do
+      assigns(:course).title.should == Settings.courses[:default_title]
+    end
+
+    it "assigns the default description" do
+      assigns(:course).description.should == Settings.courses[:default_description]
+    end
+  end
+end
