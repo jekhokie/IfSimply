d9657bbdce54aff15b7311d3d3c099dab8da7fac
Justin Karimi <jekhokie@gmail.com>
2013-05-18 20:38:47 -0400

    Update Ability for User Course Edit
    Update the Ability model to ensure that the Course edit is managed
    correctly.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    can :update, Club,   :user_id => user.id
-    can :create, Course, :club_id => user.clubs.first.id # TODO: Figure out how to restrict to course_id in user.clubs.map(&:id)
+    can :update,            Club,   :user_id => user.id
+    can [ :create, :edit ], Course, :club_id => user.clubs.first.id # TODO: Figure out how to restrict to course_id in user.clubs.map(&:id)
   end
 end
