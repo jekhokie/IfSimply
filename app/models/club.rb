98441c702504ce20b0423d87b16c4433222b0bce
Justin Karimi <jekhokie@gmail.com>
2013-05-21 08:02:14 -0400

    Update Club Model for Logo Paperclip
    Update the Club model to specify validations/etc. for the logo attribute
    being a Paperclip type.

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,13 +1,17 @@
 class Club < ActiveRecord::Base
   attr_accessible :name, :description, :price_cents, :logo
 
+  has_attached_file :logo, :styles      => { :medium => "256x256>", :thumb => "100x100>" },
+                           :default_url => Settings.clubs[:default_logo]
+
   monetize :price_cents
 
   validates :name,        :presence => { :message => "for club can't be blank" }
   validates :description, :presence => { :message => "for club can't be blank" }
-  validates :logo,        :presence => true
   validates :price_cents, :presence => true
   validates :user_id,     :presence => true
+
+  validates_attachment_content_type :logo, :content_type => [ 'image/jpeg', 'image/gif', 'image/png', 'image/tiff' ]
 
   validates_numericality_of :price_cents, :greater_than_or_equal_to => Settings.clubs[:min_price_cents],
                                           :message                  => "must be at least $#{Settings.clubs[:min_price_cents]/100}"
@@ -18,7 +22,6 @@ class Club < ActiveRecord::Base
   def assign_defaults
     self.name        = Settings.clubs[:default_name]
     self.description = Settings.clubs[:default_description]
-    self.logo        = Settings.clubs[:default_logo]
     self.price_cents = Settings.clubs[:default_price_cents]
   end
 end
