0bec02e7e0ec9bf6694418bbdbfbefe5b9ec4983
Justin Karimi <jekhokie@gmail.com>
2013-05-21 21:39:13 -0400

    Add Migration for Blog free Attribute
    Add a migration and upate the schema for a Blog free attribute.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130521220241) do
+ActiveRecord::Schema.define(:version => 20130522013458) do
 
   create_table "blogs", :force => true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20130521220241) do
     t.integer  "club_id"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.boolean  "free"
   end
 
   add_index "blogs", ["club_id"], :name => "index_blogs_on_club_id"
