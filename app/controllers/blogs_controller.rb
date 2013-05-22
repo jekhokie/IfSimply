f5a8fafc1c173faf79209cee6066993ae1fb8609
Justin Karimi <jekhokie@gmail.com>
2013-05-21 21:33:11 -0400

    Update Blog Controller for update Actions
    Update the Blog controller to include all corresponding actions to
    update a Blog.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :get_blog, :only => [ :edit, :update, :change_image, :upload_image ]
 
   def create
     @club = Club.find params[:club_id]
@@ -14,10 +15,36 @@ class BlogsController < ApplicationController
   end
 
   def edit
-    @blog = Blog.find params[:id]
-
     authorize! :edit, @blog
 
     @club = @blog.club
+  end
+
+  def update
+    authorize! :update, @blog
+
+    @blog.update_attributes params[:blog]
+
+    respond_with_bip @blog
+  end
+
+  def change_image
+    authorize! :update, @blog
+  end
+
+  def upload_image
+    authorize! :update, @blog
+
+    if @blog.update_attributes params[:blog]
+      redirect_to edit_blog_path(@blog)
+    else
+      render :change_image, :formats => [ :js ]
+    end
+  end
+
+  private
+
+  def get_blog
+    @blog = Blog.find params[:id]
   end
 end
