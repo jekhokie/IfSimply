efa65261f6edd829ac1364561f7b03b77bc4b464
Justin Karimi <jekhokie@gmail.com>
2013-05-24 10:43:57 -0400

    Add Topics Controller with new and create Actions
    Add a Topics controller that includes the new and create actions.

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,31 @@
+class TopicsController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :get_discussion_board
+
+  def new
+    authorize! :update, @discussion_board
+
+    @topic = @discussion_board.topics.new
+  end
+
+  def create
+    authorize! :update, @discussion_board
+
+    @topic = @discussion_board.topics.new params[:topic]
+
+    if @topic.save
+      flash[:notice] = "Topic posted successfully"
+      redirect_to edit_discussion_board_path(@discussion_board)
+    else
+      render :new
+    end
+
+    flash.discard
+  end
+
+  private
+
+  def get_discussion_board
+    @discussion_board = DiscussionBoard.find params[:discussion_board_id]
+  end
+end
