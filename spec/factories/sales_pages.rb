2355e37c9affc4d2e9f32945e059b85ff46f7093
Justin Karimi <jekhokie@gmail.com>
2013-06-12 09:48:57 -0400

    Update Lesson and SalesPage Factories
    Update the Lesson and SalesPage factories to ensure the video attribute
    is valid.

--- spec/factories/sales_pages.rb
+++ spec/factories/sales_pages.rb
@@ -1,5 +1,3 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :sales_page do
     club
@@ -7,5 +5,6 @@ FactoryGirl.define do
     heading        { Faker::Lorem.words(rand(3) + 3).join " " }
     sub_heading    { Faker::Lorem.words(rand(5) + 3).join " " }
     call_to_action { Faker::Lorem.words(rand(3) + 1).join " " }
+    video          { "http://www.google.com/" }
   end
 end
