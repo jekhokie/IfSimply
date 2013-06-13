641ac799d53e263205de3196871870eccb13441c
Justin Karimi <jekhokie@gmail.com>
2013-06-13 14:32:43 -0400

    Add SalesPage update Spec
    Update the SalesPage controller to include testing the update action.

--- spec/controllers/sales_pages_controller_spec.rb
+++ spec/controllers/sales_pages_controller_spec.rb
@@ -47,4 +47,62 @@ describe SalesPagesController do
       assigns(:sales_page).should == sales_page
     end
   end
+
+  describe "PUT 'update'" do
+    let!(:user)        { FactoryGirl.create :user }
+    let!(:sales_page)  { user.clubs.first.sales_page }
+    let!(:new_heading) { "Test Heading" }
+
+    before :each do
+      @request.env["devise.mapping"] = Devise.mappings[:users]
+      sign_in user
+    end
+
+    describe "for valid attributes" do
+      before :each do
+        put 'update', :id => sales_page.id, :sales_page => { :heading => new_heading }
+      end
+
+      it "returns http success" do
+        response.should be_success
+      end
+
+      it "returns the club" do
+        assigns(:club).should_not be_nil
+      end
+
+      it "returns the sales_page" do
+        assigns(:sales_page).should == sales_page
+      end
+
+      it "assigns the new attributes" do
+        sales_page.reload
+        sales_page.heading.should == new_heading
+      end
+    end
+
+    describe "for invalid attributes" do
+      before :each do
+        @old_heading = sales_page.heading
+        put 'update', :id => sales_page.id, :sales_page => { :heading => "" }
+      end
+
+      it "returns http unprocessable" do
+        response.response_code.should == 422
+      end
+
+      it "returns the club" do
+        assigns(:club).should_not be_nil
+      end
+
+      it "returns the sales_page" do
+        assigns(:sales_page).should == sales_page
+      end
+
+      it "does not update the attributes" do
+        sales_page.reload
+        sales_page.heading.should == @old_heading
+      end
+    end
+  end
 end
