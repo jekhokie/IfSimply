65af6a45ea101e79b89fa3b304ea9faff48461cc
Justin Karimi <jekhokie@gmail.com>
2013-06-13 08:47:11 -0400

    Update SalesPage Controller Spec for edit
    Add edit action SalesPage controller tests.

--- spec/controllers/sales_pages_controller_spec.rb
+++ spec/controllers/sales_pages_controller_spec.rb
@@ -2,10 +2,37 @@ require 'spec_helper'
 
 describe SalesPagesController do
   describe "GET 'show'" do
-    let!(:sales_page) { FactoryGirl.create :sales_page }
+    let!(:club)       { FactoryGirl.create :club }
+    let!(:sales_page) { club.sales_page }
 
     before :each do
       get 'show', :club_id => sales_page.club.id
+    end
+
+    it "returns http success" do
+      response.should be_success
+    end
+
+    it "returns the club" do
+      assigns(:club).should == sales_page.club
+    end
+
+    it "returns the sales_page" do
+      assigns(:sales_page).should == sales_page
+    end
+  end
+
+  describe "GET 'edit'" do
+    let!(:user)       { FactoryGirl.create :user }
+    let!(:sales_page) { user.clubs.first.sales_page }
+
+    before :each do
+      @request.env["devise.mapping"] = Devise.mappings[:users]
+      sign_in user
+    end
+
+    before :each do
+      get 'edit', :club_id => sales_page.club.id
     end
 
     it "returns http success" do
