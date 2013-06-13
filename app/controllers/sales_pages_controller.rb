a92c34d8e6b36963f8134f107aa84af92201ed07
Justin Karimi <jekhokie@gmail.com>
2013-06-13 08:48:03 -0400

    Add edit Action for SalesPage Controller
    Update the SalesPage controller to include an edit action.

--- app/controllers/sales_pages_controller.rb
+++ app/controllers/sales_pages_controller.rb
@@ -1,8 +1,19 @@
 class SalesPagesController < ApplicationController
+  before_filter :authenticate_user!, :except => [ :show ]
+  before_filter :get_club_and_sales_page
+
   def show
+    authorize! :read, @sales_page
+  end
+
+  def edit
+    authorize! :update, @sales_page
+  end
+
+  private
+
+  def get_club_and_sales_page
     @club       = Club.find params[:club_id]
     @sales_page = @club.sales_page
-
-    authorize! :read, @sales_page
   end
 end
