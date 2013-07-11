89ce72f74a8c367c4c4c648e1cb0582c5b0caa74
Justin Karimi <jekhokie@gmail.com>
2013-07-10 21:08:29 -0400

    Add Spec for Club sub_heading
    Add spec tests for the Club sub_heading attribute.

--- spec/factories/clubs.rb
+++ spec/factories/clubs.rb
@@ -2,7 +2,8 @@ FactoryGirl.define do
   factory :club do
     user
 
-    name        { Faker::Lorem.words(rand(3) + 1).join " " }
+    name        { Faker::Lorem.words(2).join " " }
+    sub_heading { Faker::Lorem.words(2).join " " }
     description { Faker::Lorem.sentence }
     price_cents { rand(9000) + 1000 }
   end
--- spec/models/club_spec.rb
+++ spec/models/club_spec.rb
@@ -22,6 +22,10 @@ describe Club do
       club.name.should == Settings.clubs[:default_name]
     end
 
+    it "assigns the default sub_heading" do
+      club.sub_heading.should == Settings.clubs[:default_sub_heading]
+    end
+
     it "assigns the default description" do
       club.description.should == Settings.clubs[:default_description]
     end
@@ -51,8 +55,17 @@ describe Club do
       FactoryGirl.build(:club, :name => "").should_not be_valid
     end
 
-    it "returns false when name is greater than 20 characters" do
-      FactoryGirl.build(:club, :name => "Something Greater than 20 Characters").should_not be_valid
+    it "returns false when name is greater than max characters" do
+      FactoryGirl.build(:club, :name => "Something Greater than max Characters").should_not be_valid
+    end
+
+    # sub_heading
+    it "returns false when no sub_heading is specified" do
+      FactoryGirl.build(:club, :sub_heading => "").should_not be_valid
+    end
+
+    it "returns false when name is greater than max characters" do
+      FactoryGirl.build(:club, :sub_heading => "Something Greater than max Characters").should_not be_valid
     end
 
     # description
