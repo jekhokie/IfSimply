85eca444fcc5a05f427d588cb89f6d63ddcc2321
Justin Karimi <jekhokie@gmail.com>
2013-07-13 18:58:04 -0400

    Add Test for Enumeration of Membership Type
    Add a test to ensure that the ClubsUsers 'type' attribute falls within
    the expected realm of membership types.

--- spec/models/clubs_users_spec.rb
+++ spec/models/clubs_users_spec.rb
@@ -4,6 +4,8 @@ describe ClubsUsers do
   it { should belong_to :club }
   it { should belong_to :user }
 
+  it { should ensure_inclusion_of(:type).in_array [ :basic, :pro ] }
+
   it "can be instantiated" do
     ClubsUsers.new.should be_an_instance_of(ClubsUsers)
   end
