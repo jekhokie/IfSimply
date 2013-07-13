dc5525b7404de595d69f5911b065f8620923d9f5
Justin Karimi <jekhokie@gmail.com>
2013-07-13 18:53:33 -0400

    Add ClubsUsers Migration
    Add a migration and update the schema to include the ClubsUsers join
    table to handle Memberships.

---
+++ db/migrate/20130713224853_create_clubs_users.rb
@@ -0,0 +1,15 @@
+class CreateClubsUsers < ActiveRecord::Migration
+  def change
+    create_table :clubs_users do |t|
+      t.string :type
+
+      t.references :user, :null => false
+      t.references :club, :null => false
+
+      t.timestamps
+    end
+
+    add_index :clubs_users, :club_id
+    add_index :clubs_users, :user_id
+  end
+end
