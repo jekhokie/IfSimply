15116641437c66973001b5c423448978ace921f5
Justin Karimi <jekhokie@gmail.com>
2013-07-13 20:30:57 -0400

    Update Routing for SessionsController Override
    Update the routing file to include an override for the default
    SessionsController.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Ifsimply::Application.routes.draw do
-  devise_for :users, :controllers => { :registrations => 'registrations' }
+  devise_for :users, :controllers => { :registrations => 'registrations', :sessions => 'sessions' }
 
   root :to => 'home#index'
 
