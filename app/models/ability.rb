206a24d1ad1dc07dd66d012312af682e3aac8c58
Justin Karimi <jekhokie@gmail.com>
2013-07-19 10:49:36 -0400

    Update Ability for create Post
    Update the Ability model to ensure that the create action is handled
    accordingly.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -57,6 +57,11 @@ class Ability
       topic.club.user == user or topic.club.members.include?(user)
     end
 
+    can :create, Post do |post|
+      membership = user.subscriptions.find_by_club_id post.club.id
+      post.club.user == user or (!membership.blank? and membership.level.to_s != "basic")
+    end
+
     # global defaults
     can [ :read ], SalesPage
   end
