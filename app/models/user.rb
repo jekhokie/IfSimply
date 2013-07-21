d398aba9eacc0c008ae11e31e1a97c5bbad1f987
Justin Karimi <jekhokie@gmail.com>
2013-07-21 12:34:57 -0400

    Update User Model for description and icon
    Update the User model to include and validate the description and icon
    attributes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,7 @@ class User < ActiveRecord::Base
 
   after_create :create_club
 
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-
-  validates :name,  :presence => true, :uniqueness => true
-  validates :email, :presence => true, :uniqueness => true
+  attr_accessible :name, :description, :email, :password, :password_confirmation, :remember_me
 
   has_many :clubs, :dependent => :destroy
 
@@ -16,8 +13,21 @@ class User < ActiveRecord::Base
 
   has_many :subscriptions, :class_name => ClubsUsers
 
+  has_attached_file :icon, :styles      => { :medium => "256x256>", :thumb => "100x100>" },
+                           :default_url => Settings.users[:default_icon]
+
+  validates :name,        :presence => true, :uniqueness => true
+  validates :email,       :presence => true, :uniqueness => true
+  validates :description, :presence => { :message => "for user can't be blank" }
+
+  validates_attachment_content_type :icon, :content_type => [ 'image/jpeg', 'image/gif', 'image/png', 'image/tiff' ]
+
   def memberships
     Club.find subscriptions.map(&:club_id)
+  end
+
+  def assign_defaults
+    self.description = Settings.users[:default_description]
   end
 
   private
