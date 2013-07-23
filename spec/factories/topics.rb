eaa7594dffa51965f8bd8aefd2a888fa6e9e7323
Justin Karimi <jekhokie@gmail.com>
2013-07-23 09:14:09 -0400

    Update Factories for Testing Topics user_id
    Update the corresponding factories to ensure they are consistent with
    the addition of the user_id attribute for the Topic model.

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :topic do
     discussion_board
 
+    user_id     { discussion_board.user.id }
     subject     { Faker::Lorem.words(rand(3) + 3).join " " }
     description { Faker::Lorem.sentences(rand(10) + 5).join " " }
   end
