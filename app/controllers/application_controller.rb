43b79b99a29fd959e192f6a7386d11d8534679d6
Justin Karimi <jekhokie@gmail.com>
2013-07-25 07:39:06 -0400

    Update Violation Page for Error Message
    Update the Violation page to ensure the error is displayed in the view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    @exception = exception
+    @violation = exception
     render :template => 'home/access_violation', :status => 403
   end
 end
