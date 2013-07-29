77644ded33b77bc5a88712522ef76a6d431cda8c
Justin Karimi <jekhokie@gmail.com>
2013-07-29 19:22:26 -0400

    Add is_editing? to ApplicationController
    Update the ApplicationController to include an is_editing? method that
    determines whether a user is currently using Mercury to edit a page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  helper_method :is_editing?
+
   rescue_from CanCan::AccessDenied do |exception|
     @violation = exception
     render :template => 'home/access_violation', :status => 403
@@ -15,5 +17,11 @@ class ApplicationController < ActionController::Base
 
     response.headers["X-Flash-Error"] = errors_string
     render :text => "error", :status => 422
+  end
+
+  private
+
+  def is_editing?
+    params[:mercury_frame].blank? ? false : true
   end
 end
