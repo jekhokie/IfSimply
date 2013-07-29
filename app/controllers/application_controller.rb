ccf266134683938e3098f94b539b37cff7d3fe0e
Justin Karimi <jekhokie@gmail.com>
2013-07-28 21:31:11 -0400

    Update Mercury Errors for Multiple Resources
    Update the Mercury error helper method to include multiple resources for
    many errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,14 @@ class ApplicationController < ActionController::Base
     render :template => 'home/access_violation', :status => 403
   end
 
-  def respond_error_to_mercury(resource)
-    response.headers["X-Flash-Error"] = resource.errors.full_messages.join("\n")
+  def respond_error_to_mercury(resources)
+    errors_string = ""
+
+    resources.each do |resource|
+      errors_string += resource.errors.full_messages.join("\n")
+    end
+
+    response.headers["X-Flash-Error"] = errors_string
     render :text => "error", :status => 422
   end
 end
