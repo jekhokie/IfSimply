6f7961cf98f6bc41e53d59ac68208856fb216db0
Justin Karimi <jekhokie@gmail.com>
2013-07-29 19:25:10 -0400

    Update SalesPages Controller for Editor Capability
    Update the SalesPages controller to include the corresponding
    actions/updates utilizing the Mercury editor.

--- app/controllers/sales_pages_controller.rb
+++ app/controllers/sales_pages_controller.rb
@@ -11,20 +11,35 @@ class SalesPagesController < ApplicationController
 
   def edit
     authorize! :update, @sales_page
+
+    render :text => '', :layout => "mercury"
   end
 
   def update
     authorize! :update, @sales_page
 
-    @sales_page.update_attributes params[:sales_page]
+    sales_page_hash            = params[:content]
+    @sales_page.heading        = sales_page_hash[:sales_page_heading][:value]
+    @sales_page.sub_heading    = sales_page_hash[:sales_page_sub_heading][:value]
+    @sales_page.video          = sales_page_hash[:sales_page_video_url][:value]
+    @sales_page.call_to_action = sales_page_hash[:sales_page_call_to_action][:value]
+    @sales_page.benefit1       = sales_page_hash[:sales_page_benefit1][:value]
+    @sales_page.benefit2       = sales_page_hash[:sales_page_benefit2][:value]
+    @sales_page.benefit3       = sales_page_hash[:sales_page_benefit3][:value]
 
-    respond_with_bip @sales_page
+    @club.price = sales_page_hash[:club_price][:value]
+
+    if @sales_page.save and @club.save
+      render :text => ""
+    else
+      respond_error_to_mercury [ @sales_page, @club ]
+    end
   end
 
   private
 
   def get_club_and_sales_page
-    @sales_page = SalesPage.find params[:id]
-    @club       = @sales_page.club
+    @club       = Club.find params[:club_id]
+    @sales_page = @club.sales_page
   end
 end
