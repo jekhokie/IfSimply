c5570c66108071bb69f8e0df33b558984cdd7fde
Justin Karimi <jekhokie@gmail.com>
2013-07-29 21:59:11 -0400

    Spec and Update Ability for read User
    Add a spec for and update the Ability model to allow for read capability
    for the User model.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -30,7 +30,7 @@ class Ability
       sales_page.user == user
     end
 
-    can [ :update ], User, :id => user.id
+    can [ :read, :update ], User, :id => user.id
 
     # membership-specific capabilities
     can :read, Club do |club|
