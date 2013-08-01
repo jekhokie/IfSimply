d3db924233a42e2da086adb89cc1d3293bd5c476
Justin Karimi <jekhokie@gmail.com>
2013-07-31 20:35:07 -0400

    Update Users Controller Spec for Mercury
    Update the Users controller spec to include the correct tests/situations
    for integrating the Mercury editor.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,63 @@ require 'spec_helper'
 describe UsersController do
   let(:user) { FactoryGirl.create :user }
 
+  describe "GET 'show'" do
+    describe "for a signed-in user" do
+      before :each do
+        @request.env["devise.mapping"] = Devise.mappings[:users]
+        sign_in user
+      end
+
+      describe "viewing their own profile" do
+        before :each do
+          get 'show', :id => user.id
+        end
+
+        it "returns http success" do
+          response.should be_success
+        end
+
+        it "renders the user show view" do
+          response.should render_template("users/show")
+        end
+
+        it "returns the user" do
+          assigns(:user).should_not be_nil
+        end
+      end
+
+      describe "viewing a different user's profile" do
+        let!(:other_user) { FactoryGirl.create :user }
+
+        before :each do
+          get 'show', :id => other_user.id
+        end
+
+        it "returns http success" do
+          response.should be_success
+        end
+
+        it "renders the user show view" do
+          response.should render_template("users/show")
+        end
+
+        it "returns the user" do
+          assigns(:user).should_not be_nil
+        end
+      end
+    end
+
+    describe "for a non signed-in user" do
+      before :each do
+        get 'show', :id => user.id
+      end
+
+      it "returns redirect to sign in" do
+        response.should redirect_to(new_user_session_path)
+      end
+    end
+  end
+
   describe "GET 'edit'" do
     describe "for a signed-in user" do
       before :each do
@@ -19,12 +76,12 @@ describe UsersController do
           response.should be_success
         end
 
-        it "renders the users edit view" do
-          response.should render_template("users/edit")
-        end
-
         it "assigns user" do
           assigns(:user).should == user
+        end
+
+        it "renders the mercury layout" do
+          response.should render_template(:layout => "layouts/mercury")
         end
       end
 
@@ -35,16 +92,12 @@ describe UsersController do
           get 'edit', :id => other_user.id
         end
 
-        it "returns http success" do
-          response.should be_success
+        it "returns http forbidden" do
+          response.response_code.should == 403
         end
 
-        it "renders the users edit view" do
-          response.should render_template("users/edit")
-        end
-
-        it "assigns user as their own account" do
-          assigns(:user).should == user
+        it "renders the access violation view" do
+          response.should render_template("home/access_violation")
         end
       end
     end
@@ -72,7 +125,8 @@ describe UsersController do
       describe "updating their own attributes" do
         describe "for valid attributes" do
           before :each do
-            put 'update', :id => user.id, :user => { :description => new_description }
+            put 'update', :id => user.id, :content => { :user_icon        => { :attributes => { :src => "abc" } },
+                                                        :user_description => { :value => new_description } }
           end
 
           it "returns http success" do
@@ -91,11 +145,9 @@ describe UsersController do
 
         describe "for invalid attributes" do
           before :each do
-            @request.env["devise.mapping"] = Devise.mappings[:users]
-            sign_in user
-
             @old_description = user.description
-            put 'update', :id => user.id, :user => { :description => "" }
+            put 'update', :id => user.id, :content => { :user_icon        => { :attributes => { :src => "abc" } },
+                                                        :user_description => { :value => "" } }
           end
 
           it "returns http unprocessable" do
@@ -117,177 +169,20 @@ describe UsersController do
         let!(:other_user) { FactoryGirl.create :user }
 
         before :each do
-          put 'update', :id => other_user.id, :user => { :description => "Test" }
+          put 'update', :id => other_user.id, :content => { :user_icon        => { :attributes => { :src => "abc" } },
+                                                            :user_description => { :value => "123" } }
         end
 
-        it "returns http unprocessable" do
-          response.response_code.should == 204
-        end
-
-        it "assigns user as their own account" do
-          assigns(:user).should == user
+        it "returns http forbidden" do
+          response.response_code.should == 403
         end
       end
     end
 
     describe "for a non signed-in user" do
       before :each do
-        put 'update', :id => user.id, :user => { :description => "Test" }
-      end
-
-      it "redirects to the sign-in page" do
-        response.should redirect_to(new_user_session_path)
-      end
-    end
-  end
-
-  describe "GET 'change_icon'" do
-    describe "for a signed-in user" do
-      before :each do
-        @request.env["devise.mapping"] = Devise.mappings[:users]
-        sign_in user
-      end
-
-      describe "editing their own icon" do
-        before :each do
-          get 'change_icon', :id => user.id, :format => :js
-        end
-
-        it "returns http success" do
-          response.should be_success
-        end
-
-        it "returns the user" do
-          assigns(:user).should == user
-        end
-      end
-
-      describe "updating another user's icon" do
-        let!(:other_user) { FactoryGirl.create :user }
-
-        before :each do
-          get 'change_icon', :id => other_user.id, :format => :js
-        end
-
-        it "returns http unprocessable" do
-          response.should be_success
-        end
-
-        it "assigns user as their own account" do
-          assigns(:user).should == user
-        end
-      end
-    end
-
-    describe "for a non signed-in user" do
-      before :each do
-        get 'change_icon', :id => user.id, :format => :js
-      end
-
-      it "redirects to the sign-in page" do
-        response.should redirect_to(new_user_session_path)
-      end
-    end
-  end
-
-  describe "PUT 'upload_icon'" do
-    let(:valid_icon)   { fixture_file_upload('/soccer_ball.jpg', 'image/jpeg') }
-    let(:invalid_icon) { fixture_file_upload('/soccer_ball.txt', 'text/plain') }
-
-    describe "for a signed-in user" do
-      before :each do
-        @request.env["devise.mapping"] = Devise.mappings[:users]
-        sign_in user
-      end
-
-      describe "updating their own icon" do
-        describe "for a valid icon format" do
-          before :each do
-            put 'upload_icon', :id => user.id, :user => { :icon => valid_icon }, :format => :js
-          end
-
-          it "returns http success" do
-            response.should be_success
-          end
-
-          it "renders the upload_icon template" do
-            response.should render_template('users/upload_icon')
-          end
-
-          it "returns the user" do
-            assigns(:user).should == user
-          end
-
-          it "assigns the user icon" do
-            File.basename(assigns(:user).icon.to_s.sub(/\?.*/, '')).should == valid_icon.original_filename
-          end
-        end
-
-        describe "for an invalid icon format" do
-          before :each do
-            put 'upload_icon', :id => user.id, :user => { :icon => invalid_icon }, :format => :js
-          end
-
-          it "returns http success" do
-            response.should be_success
-          end
-
-          it "renders change_icon" do
-            response.should render_template("users/change_icon")
-          end
-
-          it "returns the user" do
-            assigns(:user).should == user
-          end
-
-          it "does not assign the user icon" do
-            File.basename(assigns(:user).icon.to_s.sub(/\?.*/, '')).should_not == valid_icon.original_filename
-          end
-        end
-
-        describe "for a non-specified icon value" do
-          before :each do
-            put 'upload_icon', :id => user.id, :format => :js
-          end
-
-          it "returns http success" do
-            response.should be_success
-          end
-
-          it "renders change_icon" do
-            response.should render_template("users/change_icon")
-          end
-
-          it "returns the user" do
-            assigns(:user).should == user
-          end
-
-          it "does not assign the user icon" do
-            File.basename(assigns(:user).icon.to_s.sub(/\?.*/, '')).should_not == valid_icon.original_filename
-          end
-        end
-      end
-
-      describe "uploading an icon for another user" do
-        let!(:other_user) { FactoryGirl.create :user }
-
-        before :each do
-          put 'upload_icon', :id => other_user.id, :user => { :icon => invalid_icon }, :format => :js
-        end
-
-        it "returns http unprocessable" do
-          response.should be_success
-        end
-
-        it "assigns user as their own account" do
-          assigns(:user).should == user
-        end
-      end
-    end
-
-    describe "for a non signed-in user" do
-      before :each do
-        put 'upload_icon', :id => user.id, :format => :js
+        put 'update', :id => user.id, :content => { :user_icon        => { :attributes => { :src => "abc" } },
+                                                    :user_description => { :value => "123" } }
       end
 
       it "redirects to the sign-in page" do
