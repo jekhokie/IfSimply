ca62d4f05291383dcfd7e546b1c2c49d3017acbc
Justin Karimi <jekhokie@gmail.com>
2013-08-08 15:35:34 -0400

    Add Hash for File Upload Using Paperclip
    Update the Paperclip model to ensure that the files being uploaded are
    obfuscated so that they cannot be easily downloaded/discovered.

--- app/models/mercury/image.rb
+++ app/models/mercury/image.rb
@@ -4,8 +4,9 @@ class Mercury::Image < ActiveRecord::Base
   attr_accessible :image
 
   has_attached_file :image, :styles      => { :medium => "300x300>", :thumb => "100x100>", :icon => "50x50>" },
-                            :path        => ":rails_root/public/system/:attachment/:id/:style/:filename",
-                            :url         => "/system/:attachment/:id/:style/:filename",
+                            :path        => ":rails_root/public/system/:attachment/:attachment_id/:id/:style/:hash.:extension",
+                            :url         => "/system/:attachment/:attachment_id/:id/:style/:hash.:extension",
+                            :hash_secret => "soighow3th2t8hgo2ih#JT(T#y09yt09#)(TY#)(TY)#(G()GVh03gh)(QY(@)YRT()G#H#()g",
                             :default_url => Settings.clubs[:default_logo]
 
   validates_attachment_content_type :image, :content_type => [ 'image/jpeg', 'image/gif', 'image/png', 'image/tiff' ]
