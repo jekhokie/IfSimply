c8986861a415f330d44ec97f8e2e1ffc92fc10f2
Justin Karimi <jekhokie@gmail.com>
2013-08-25 14:51:15 -0400

    Update User Factory for [verified,unverified]_user
    Update the User FactoryGirl to create additional user types
    (verified_user and unverified_user).

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,5 +7,16 @@ FactoryGirl.define do
     confirmed_at          { Time.now }
 
     description { Faker::Lorem.sentences(rand(5) + 5).join " " }
+
+    trait :verified do
+      verified true
+    end
+
+    trait :unverified do
+      verified false
+    end
+
+    factory :verified_user,   traits: [ :verified ]
+    factory :unverified_user, traits: [ :unverified ]
   end
 end
