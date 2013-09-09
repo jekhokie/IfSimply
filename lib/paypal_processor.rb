93946479351bf4f2d5a34d818be7bad577b72e94
Justin Karimi <jekhokie@gmail.com>
2013-09-09 19:57:54 -0400

    Update the PaypalProcessor Lib for preapproval_url
    Update the PaypalProcessor lib to include a method to return the
    preapproval URL (or blank string in the case of an error).

--- lib/paypal_processor.rb
+++ lib/paypal_processor.rb
@@ -18,4 +18,46 @@ module PaypalProcessor
       return false
     end
   end
+
+  def self.request_preapproval_url(monthly_amount, cancel_url, return_url, ipn_url, member_name, club_name)
+    return "" if monthly_amount.blank?
+    return "" if cancel_url.blank?
+    return "" if return_url.blank?
+    return "" if ipn_url.blank?
+    return "" if member_name.blank?
+    return "" if club_name.blank?
+
+    @api = PayPal::SDK::AdaptivePayments::API.new
+
+    # Build request object
+    @preapproval = @api.build_preapproval({
+      :clientDetails => {
+        :applicationId => Settings.general[:default_application_name],
+        :customerId    => member_name,
+        :customerType  => "Member",
+        :partnerName   => Settings.general[:default_partner_name]
+      },
+      :memo                         => "Membership To: #{club_name}",
+      :cancelUrl                    => cancel_url,
+      :currencyCode                 => "USD",
+      :maxAmountPerPayment          => monthly_amount,
+      :maxNumberOfPaymentsPerPeriod => 1,
+      :paymentPeriod                => "MONTHLY",
+      :returnUrl                    => return_url,
+      :requireInstantFundingSource  => true,
+      :ipnNotificationUrl           => ipn_url,
+      :startingDate                 => DateTime.now,
+      :feesPayer                    => "PRIMARYRECEIVER",
+      :displayMaxTotalAmount        => true })
+
+    # Make API call & get response
+    @preapproval_response = @api.preapproval(@preapproval)
+
+    # Access Response
+    if @preapproval_response.success?
+      @api.preapproval_url(@preapproval_response) # Url to complete payment
+    else
+      ""
+    end
+  end
 end
