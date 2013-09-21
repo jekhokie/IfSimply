c9fc296b2bc11af986a90e05d2abc23cb2ea1952
Justin Karimi <jekhokie@gmail.com>
2013-09-21 12:25:00 -0400

    Update ClubsUsers Controller Spec for PayPal
    Update the ClubsUsers controller spec to ensure that the preapproval key
    is assigned when a successful preapproval request returns from PayPal.

--- spec/controllers/clubs_users_controller_spec.rb
+++ spec/controllers/clubs_users_controller_spec.rb
@@ -151,13 +151,13 @@ describe ClubsUsersController do
 
       describe "for an existing subscription" do
         let!(:subscribing_user)      { FactoryGirl.create :user }
-        let!(:existing_subscription) { FactoryGirl.create :subscription, :user => subscribing_user, :club => club }
+        let!(:existing_subscription) { FactoryGirl.create :subscription, :user => subscribing_user, :club => club, :level => :basic }
 
         before :each do
           @request.env["devise.mapping"] = Devise.mappings[:users]
           sign_in subscribing_user
 
-          post 'create', :id => club.id
+          post 'create', :id => club.id, :level => 'basic'
         end
 
         it "redirects to the club show view" do
@@ -167,16 +167,111 @@ describe ClubsUsersController do
         it "returns the club" do
           assigns(:club).should == club
         end
+      end
 
-        it "adds the user as a free subscriber to the club" do
+      describe "for a non signed-in user" do
+        before :each do
+          post 'create', :id => club.id, :level => 'basic'
+        end
+
+        it "redirects to the sales page" do
+          response.should redirect_to(club_sales_page_path(club))
+        end
+
+        it "returns the club" do
+          assigns(:club).should == club
+        end
+
+        it "returns the sales_page" do
+          assigns(:sales_page).should == club.sales_page
+        end
+      end
+    end
+
+    describe "for a pro membership subscription" do
+      let!(:subscribing_user) { FactoryGirl.create :user }
+      let!(:preapproval_hash) { { :preapproval_key => "PA-5W790039F30657208", :preapproval_url => "http://sandbox.paypal.com" } }
+
+      describe "for a new subscription" do
+        before :each do
+          @request.env["devise.mapping"] = Devise.mappings[:users]
+          sign_in subscribing_user
+
+          PaypalProcessor.should_receive(:request_preapproval).and_return preapproval_hash
+
+          post 'create', :id => club.id, :level => 'pro'
+        end
+
+        it "returns a redirect" do
+          response.should be_redirect
+        end
+
+        it "returns the club" do
+          assigns(:club).should == club
+        end
+
+        it "returns the subscription" do
+          assigns(:subscription).should_not be_blank
+        end
+
+        it "assigns the preapproval for the subscription" do
+          assigns(:subscription).preapproval_key.should == preapproval_hash[:preapproval_key]
+        end
+
+        it "does not add the subscriber as a member of the club" do
           club.reload
-          club.members.should include(subscribing_user)
+          club.members.should_not include(subscribing_user)
+        end
+      end
+
+      describe "for an existing subscription" do
+        let!(:subscribing_user)      { FactoryGirl.create :user }
+        let!(:existing_subscription) { FactoryGirl.create :subscription, :user => subscribing_user, :club => club, :level => :pro }
+
+        before :each do
+          @request.env["devise.mapping"] = Devise.mappings[:users]
+          sign_in subscribing_user
+
+          PaypalProcessor.should_not_receive(:request_preapproval)
+
+          post 'create', :id => club.id, :level => 'pro'
+        end
+
+        it "redirects to the club show view" do
+          response.should be_redirect
+        end
+
+        it "returns the club" do
+          assigns(:club).should == club
+        end
+      end
+
+      describe "for a blank url returned from the paypal request" do
+        before :each do
+          @request.env["devise.mapping"] = Devise.mappings[:users]
+          sign_in subscribing_user
+
+          PaypalProcessor.should_receive(:request_preapproval).and_return {}
+
+          post 'create', :id => club.id, :level => 'pro'
+        end
+
+        it "renders new" do
+          response.should render_template("clubs_users/new")
+        end
+
+        it "returns an error flash" do
+          flash[:error].should_not be_blank
+        end
+
+        it "returns the club" do
+          assigns(:club).should == club
         end
       end
 
       describe "for a non signed-in user" do
         before :each do
-          post 'create', :id => club.id
+          post 'create', :id => club.id, :level => 'pro'
         end
 
         it "redirects to the sales page" do
