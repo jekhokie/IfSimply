5cd9b9ebc0d9d62441827814a2bb74244363a932
Justin Karimi <jekhokie@gmail.com>
2013-11-07 19:43:01 -0500

    Update SalesPage Specs for Details
    Update the SalesPage spec and factory to include testing for the details
    attribute.

--- spec/factories/sales_pages.rb
+++ spec/factories/sales_pages.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
     benefit1       { Faker::Lorem.words(rand(5) + 1).join " " }
     benefit2       { Faker::Lorem.words(rand(5) + 1).join " " }
     benefit3       { Faker::Lorem.words(rand(5) + 1).join " " }
+    details        { Faker::Lorem.words(rand(5) + 1).join " " }
     call_to_action { Faker::Lorem.words(rand(3) + 1).join " " }
     heading        { Faker::Lorem.words(rand(3) + 3).join " " }
     sub_heading    { Faker::Lorem.words(rand(5) + 3).join " " }
