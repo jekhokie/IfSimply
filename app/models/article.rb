ffb54b50ab914e9cc19698350718384fcb3e51bf
Justin Karimi <jekhokie@gmail.com>
2013-11-13 12:12:14 -0500

    Update Article and Defaults
    Update the settings to reference Article instead of Blog, and ensure the
    Article model has the correct associations.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,10 @@
-class Blog < ActiveRecord::Base
+class Article < ActiveRecord::Base
   attr_accessible :content, :free, :image, :title
 
   default_scope order('created_at ASC')
 
-  validates :title,   :presence => { :message => "for blog can't be blank" }
-  validates :content, :presence => { :message => "for blog can't be blank" }
+  validates :title,   :presence => { :message => "for article can't be blank" }
+  validates :content, :presence => { :message => "for article can't be blank" }
 
   validate  :free_is_valid
 
@@ -19,17 +19,17 @@ class Blog < ActiveRecord::Base
   end
 
   def assign_defaults
-    self.title   = Settings.blogs[:default_title]
-    self.content = Settings.blogs[:default_content]
-    self.free    = Settings.blogs[:default_free]
-    self.image   = Settings.blogs[:default_image]
+    self.title   = Settings.articles[:default_title]
+    self.content = Settings.articles[:default_content]
+    self.free    = Settings.articles[:default_free]
+    self.image   = Settings.articles[:default_image]
   end
 
   private
 
   def free_is_valid
     unless free.to_s =~ /(true|false)/
-      errors.add(:base, "Free for blog must be either true or false")
+      errors.add(:base, "Free for article must be either true or false")
     end
   end
 end
