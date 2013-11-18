9e7a6de2257f8f42f522c0a452ebeb7aec7cf334
Justin Karimi <jekhokie@gmail.com>
2013-11-18 18:11:50 -0500

    Remove Blog Initializer Migration
    Remove the initializer for Blog as this will never happen for new
    instances.

--- db/migrate/20130805234516_change_blogs_image_to_string.rb
+++ db/migrate/20130805234516_change_blogs_image_to_string.rb
@@ -2,8 +2,6 @@ class ChangeBlogsImageToString < ActiveRecord::Migration
   def up
     remove_attachment :blogs, :image
     add_column :blogs, :image, :string
-
-    Blog.all.each{|b| b.update_attributes :image => "/assets/iS_club_thumb.jpg"}
   end
 
   def down
