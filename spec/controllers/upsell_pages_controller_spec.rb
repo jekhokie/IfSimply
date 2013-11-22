0b4fe213e10901a08e47b8e06aba4a06e517c11a
Justin Karimi <jekhokie@gmail.com>
2013-11-22 16:58:19 -0500

    Add show Action to UpsellPagesController Spec
    Update the UpsellPagesController spec to include the show action,
    required by the Mercury editor.

--- spec/controllers/upsell_pages_controller_spec.rb
+++ spec/controllers/upsell_pages_controller_spec.rb
@@ -1,6 +1,56 @@
 require 'spec_helper'
 
 describe UpsellPagesController do
+  describe "GET 'show'" do
+    describe "for a Club owner with a verified PayPal account" do
+      let!(:user)        { FactoryGirl.create :user, :verified => true }
+      let!(:club)        { FactoryGirl.create :club, :user => user }
+      let!(:upsell_page) { club.upsell_page }
+
+      before :each do
+        get 'show', :club_id => upsell_page.club.id
+      end
+
+      it "returns http success" do
+        response.should be_success
+      end
+
+      it "returns the club" do
+        assigns(:club).should == upsell_page.club
+      end
+
+      it "returns the upsell_page" do
+        assigns(:upsell_page).should == upsell_page
+      end
+    end
+
+    describe "for a Club owner with an unverified PayPal account" do
+      let!(:user)        { FactoryGirl.create :user, :verified => false }
+      let!(:club)        { FactoryGirl.create :club, :user => user }
+      let!(:upsell_page) { club.upsell_page }
+
+      before :each do
+        get 'show', :club_id => upsell_page.club.id
+      end
+
+      it "redirects to the root path" do
+        response.should redirect_to(root_path)
+      end
+
+      it "returns the club" do
+        assigns(:club).should == upsell_page.club
+      end
+
+      it "returns the upsell_page" do
+        assigns(:upsell_page).should == upsell_page
+      end
+
+      it "returns a flash error" do
+        flash[:error].should_not be_blank
+      end
+    end
+  end
+
   describe "GET 'edit'" do
     let!(:user)        { FactoryGirl.create :user }
     let!(:upsell_page) { user.clubs.first.upsell_page }
