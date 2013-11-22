df19bb3287fed21c41acb4ccb574f67d3f23f33e
Justin Karimi <jekhokie@gmail.com>
2013-11-21 19:35:44 -0500

    Update UpsellPage Factory for Club Association
    Add the club association to the UpsellPage Factory.

--- spec/factories/upsell_pages.rb
+++ spec/factories/upsell_pages.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :upsell_page do
+    club
+
     heading                 { Faker::Lorem.words(rand(5) + 1).join " " }
     sub_heading             { Faker::Lorem.words(rand(5) + 1).join " " }
     basic_articles_desc     { Faker::Lorem.words(rand(5) + 1).join " " }
