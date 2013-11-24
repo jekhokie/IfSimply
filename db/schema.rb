37a51a403116a0211c8b32339c3f7d8480ffdf85
Justin Karimi <jekhokie@gmail.com>
2013-11-24 16:34:34 -0500

    Re-Align Schema
    Re-align the schema with a previous rebase of master.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131114010634) do
+ActiveRecord::Schema.define(:version => 20131121233356) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
