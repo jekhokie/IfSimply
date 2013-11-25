a02b0b865cc7c9437107ce39bcfefc8215668356
Justin Karimi <jekhokie@gmail.com>
2013-11-24 19:59:29 -0500

    Update SalesPage Factory for call_details
    Add the call_details attribute to the SalesPage model factory.

--- spec/factories/sales_pages.rb
+++ spec/factories/sales_pages.rb
@@ -2,13 +2,14 @@ FactoryGirl.define do
   factory :sales_page do
     club
 
-    benefit1       { Faker::Lorem.words(rand(5) + 1).join " " }
-    benefit2       { Faker::Lorem.words(rand(5) + 1).join " " }
-    benefit3       { Faker::Lorem.words(rand(5) + 1).join " " }
-    details        { Faker::Lorem.words(rand(5) + 1).join " " }
-    call_to_action { Faker::Lorem.words(rand(3) + 1).join " " }
-    heading        { Faker::Lorem.words(rand(3) + 3).join " " }
-    sub_heading    { Faker::Lorem.words(rand(5) + 3).join " " }
+    benefit1       { Faker::Lorem.words(rand(5) + 1).join " "  }
+    benefit2       { Faker::Lorem.words(rand(5) + 1).join " "  }
+    benefit3       { Faker::Lorem.words(rand(5) + 1).join " "  }
+    details        { Faker::Lorem.words(rand(5) + 1).join " "  }
+    call_to_action { Faker::Lorem.words(rand(3) + 1).join " "  }
+    call_details   { Faker::Lorem.words(rand(22) + 1).join " " }
+    heading        { Faker::Lorem.words(rand(3) + 3).join " "  }
+    sub_heading    { Faker::Lorem.words(rand(5) + 3).join " "  }
     video          { "http://www.ifsimply.com/" }
   end
 end
