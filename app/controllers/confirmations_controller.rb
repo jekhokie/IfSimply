12bb2b553091581f73e4fae7c747ff9cb07a682b
Justin Karimi <jekhokie@gmail.com>
2013-12-14 19:39:18 -0500

    Update Confirmations Controller for Resend
    Update the Confirmations Controller to handle custom resend of
    confirmation instructions.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,4 +1,20 @@
 class ConfirmationsController < Devise::ConfirmationsController
+  def new
+    super
+  end
+
+  def create
+    if params[:user] and params[:user][:email] and (user = User.where(:email => params[:user][:email]).first).blank?
+      flash[:alert] = "Email is not in the system"
+    elsif user.confirmed?
+      flash[:alert] = "Account is already confirmed - please try signing in"
+    else
+      super
+    end
+
+    flash.discard
+  end
+
   def show
     user = User.find_by_confirmation_token params[:confirmation_token]
 
@@ -20,5 +36,9 @@ class ConfirmationsController < Devise::ConfirmationsController
 
   def after_confirmation_path_for(resource_name, resource)
     user_editor_path(resource)
+  end
+
+  def after_resending_confirmation_instructions_path_for(resource_name)
+    after_devise_path
   end
 end
