3b660da09d2b2e36c60195ca8a25519571943359
Justin Karimi <jekhokie@gmail.com>
2013-12-14 20:06:26 -0500

    Add UnlocksController for Unlocking Account
    Add a custom UnlocksController to handle unlocking a User's account.

---
+++ app/controllers/unlocks_controller.rb
@@ -0,0 +1,23 @@
+class UnlocksController < Devise::UnlocksController
+  def new
+    super
+  end
+
+  def create
+    if params[:user] and params[:user][:email] and (user = User.where(:email => params[:user][:email]).first).blank?
+      flash[:alert] = "Email is not in the system"
+    elsif ! user.locked_at?
+      flash[:alert] = "Account is not locked - please try signing in"
+    else
+      super
+    end
+
+    flash.discard
+  end
+
+  protected
+
+  def after_sending_unlock_instructions_path_for(resource)
+    after_devise_path
+  end
+end
