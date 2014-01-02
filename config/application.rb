71a8a89d5204fb57952caa1de1bcb10203871f14
Justin Karimi <jekhokie@gmail.com>
2014-01-02 08:57:21 -0500

    Incorporate and Serve New Home Page Video
    Update views and application settings to serve web-compliant videos
    (including the Home page video).

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,9 @@ module Ifsimply
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
 
+    # Additional assets to load
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
