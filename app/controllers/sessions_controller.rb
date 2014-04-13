b4431b8612343ddac56e5db60ded7e73a3350c04
Justin Karimi <jekhokie@gmail.com>
2014-04-13 19:27:59 -0400

    Fix Login for Non-JS Requests
    Fix the SessionsController to ensure that non-JS logins function as
    expected (with a redirect).

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,16 @@ class SessionsController < Devise::SessionsController
   def new
   end
 
-  protected
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
 
-  def after_sign_in_path_for(resource)
+    # redirect user using HTML if non-JS sign-in
     if session[:subscription]
-      subscribe_to_club_path(session[:subscription].club)
+      respond_with resource, :location => subscribe_to_club_path(session[:subscription].club) and return
+    elsif session["user_return_to"]
+      render :js => "window.location = '#{session["user_return_to"]}'" and return if session["user_return_to"]
     else
       super
     end
