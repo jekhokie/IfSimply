3f0c2552c12b4491e6b4b0f4cb647115b7407a09
Justin Karimi <jekhokie@gmail.com>
2014-06-11 19:43:46 -0400

    Add Migration for Course Description Full Text
    Add a migration to categorize the Course description as a full text field.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140611232814) do
+ActiveRecord::Schema.define(:version => 20140611234626) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(:version => 20140611232814) do
 
   create_table "courses", :force => true do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.integer  "club_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
